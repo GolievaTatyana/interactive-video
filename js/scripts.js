99674d5fc4c078f3d67d3a146d820bfa00a52625
GolievaTatyana <golieva.tatyana@gmail.com>
2019-05-02 00:05:55 +0300

    project added

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function visibleButtons() {
+  var buttons = document.getElementById("btn");
+  buttons.classList.add("visible");
+};
+
+// var video = document.getElementById("video")[0];
+// if (video) {
+//   video.addEventListener("ended", visibleButtons);
+// }
