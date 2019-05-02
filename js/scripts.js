01ba0652ef7aa67a6930c116787dc0fd12edf5a0
Tanya Goleva <tanya@itsua.org>
2019-05-02 16:32:07 +0300

    events added

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,65 @@
-function visibleButtons() {
-  var buttons = document.getElementById("btn");
-  buttons.classList.add("visible");
+const video = document.getElementById("video");
+
+const stepOne = document.getElementById("step-1");
+const wayOne = document.getElementById("way-1");
+const wayTwo = document.getElementById("way-2");
+
+const stepTwo = document.getElementById("step-2");
+const wayThree = document.getElementById("way-3");
+const wayFour = document.getElementById("way-4");
+
+
+
+video.addEventListener("ended", visibleButtonsStepOne);
+wayOne.addEventListener("click", wayOneStepOne);
+wayTwo.addEventListener("click", wayTwoStepOne);
+
+wayThree.addEventListener("click", wayThreeStepTwo);
+wayFour.addEventListener("click", wayFourStepTwo);
+
+function visibleButtonsStepOne() {
+  stepOne.classList.add("visible");
+  video.removeEventListener("ended", visibleButtonsStepOne);
+  video.addEventListener("ended", visibleButtonsStepTwo);
 };
 
-// var video = document.getElementById("video")[0];
-// if (video) {
-//   video.addEventListener("ended", visibleButtons);
-// }
+function wayOneStepOne(event) {
+  event.preventDefault();
+  stepOne.classList.remove("visible");
+  video.removeEventListener("ended", visibleButtonsStepOne);
+  video.src = 'media/video2.mp4';
+  video.addEventListener("ended", visibleButtonsStepTwo);
+  video.play();
+};
+
+function wayTwoStepOne(event) {
+  event.preventDefault();
+  stepOne.classList.remove("visible");
+  video.removeEventListener("ended", visibleButtonsStepOne);
+  video.src = 'media/video3.mp4';
+  video.addEventListener("ended", visibleButtonsStepTwo);
+  video.play();
+};
+
+function visibleButtonsStepTwo() {
+  stepOne.classList.remove("visible");
+  stepTwo.classList.add("visible");
+};
+
+function wayThreeStepTwo(event) {
+  event.preventDefault();
+  stepTwo.classList.remove("visible");
+  video.removeEventListener("ended", visibleButtonsStepTwo);
+  video.src = 'media/video4.mp4';
+  video.addEventListener("ended", () => alert('!!!!'));
+  video.play();
+};
+
+function wayFourStepTwo(event) {
+  event.preventDefault();
+  stepTwo.classList.remove("visible");
+  video.removeEventListener("ended", visibleButtonsStepTwo);
+  video.src = 'media/video5.mp4';
+  video.addEventListener("ended", () => alert('!!!!'));
+  video.play();
+};
